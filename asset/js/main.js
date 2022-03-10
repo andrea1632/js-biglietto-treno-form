0bc1e21879b6d8d166fdd3ae1dcd390c41986212
andrea1632 <andrea.bellucci10@libero.it>
2022-03-10 16:20:33 +0100

    work in progress

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,11 +2,14 @@ let nome = document.getElementById("nomeInput");
 let km = document.getElementById("kmInput");
 let age = document.getElementById("ageInput");
 let bottoneGenera = document.getElementById("genera");
+let display = document.getElementById("dNone");
 
 bottoneGenera.addEventListener("click",
     function(){
         nome = nome.value
         km = km.value
         age = age.value
+        
     }    
 );
+document.getElementById("nomePasseggero").innerHTML = nome;
